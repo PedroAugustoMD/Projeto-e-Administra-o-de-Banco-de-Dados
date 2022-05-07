63126b093af625957c43b2109b1621eebc4e71e7
PedroAugustoMD <pedro.augustohp02@gmail.com>
2022-05-06 23:27:07 -0300

    Questao 12 #1

--- tarefa/t01/tarefa01-q10.sql
+++ tarefa/t01/tarefa01-q10.sql
@@ -1,3 +1,3 @@
 SELECT p.nome, d.descricao, f.nome 
 FROM projeto p, departamento d, funcionario f 
-WHERE p.cod_responsavel = f.codigo and p.cod_depto = d.codigo;
+WHERE p.cod_responsavel = f.codigo AND p.cod_depto = d.codigo;

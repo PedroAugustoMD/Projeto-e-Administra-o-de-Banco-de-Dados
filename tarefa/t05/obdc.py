92a24238a487647da998abc4a067a721df948660
PedroAugustoMD <pedro.augustohp02@gmail.com>
2022-06-30 17:34:45 -0300

    Questão 5c #5

--- tarefa/t05/obdc.py
+++ tarefa/t05/obdc.py
@@ -10,8 +10,19 @@ cur = con.cursor()
 
 cur.execute("insert into atividade(descricao, codProjeto, dataInicio, dataFim) values('BD - Atividade 4', 3, '2022-06-30', '2022-07-28');")
 con.commit()
+
 # Atualizar o líder de um projeto
 
 cur.execute("update projeto set codresponsavel = 1 where codigo = 3;")
 con.commit()
 
+# Listar todas os projetos e suas atividades
+
+cur.execute("SELECT * FROM projeto p, atividade a WHERE a.codprojeto = p.codigo GROUP BY p.codigo, a.codigo ORDER BY p.codigo ASC;")
+resultados = cur.fetchone() 
+
+for i in resultados:
+    print(i)
+
+cur.close()
+con.close()

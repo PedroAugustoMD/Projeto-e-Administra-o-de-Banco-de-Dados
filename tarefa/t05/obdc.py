bf891d08179acb3f31f2ea1553f3d2004e54e74c
PedroAugustoMD <pedro.augustohp02@gmail.com>
2022-06-30 17:19:13 -0300

    Questão 5a #5

--- tarefa/t05/obdc.py
+++ tarefa/t05/obdc.py
@@ -1,4 +1,11 @@
 import psycopg2
+
+#Conexão
+
 con = psycopg2.connect(host='localhost', database='AtividadesBD',
 user='postgres', password='postgres')
 cur = con.cursor()
+
+# Inserir uma atividade em algum projeto
+
+cur.execute("insert into atividade(descricao, codProjeto, dataInicio, dataFim) values('BD - Atividade 3', 3, '2022-06-30', '2022-07-28');")
